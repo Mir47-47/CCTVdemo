41874829a21dea6297165708e284eef936e76939
Mir47-47 <53306904+Mir47-47@users.noreply.github.com>
2025-05-01 18:42:04 +0900

    background test

--- app/src/main/java/com/example/cctv2/Service/MyForegroundService.java
+++ app/src/main/java/com/example/cctv2/Service/MyForegroundService.java
@@ -110,13 +110,6 @@ public class MyForegroundService extends Service {
                 .build();
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
         NotificationManagerCompat.from(this).notify(2, notification);
