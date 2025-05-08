ea732cebf471a0ac45cb3db3d4d8ef305b6d8233
Jinsu Kim <wlstn3697@naver.com>
2025-05-08 20:20:30 +0900

    change Setting  UI

--- app/src/main/java/com/example/cctv2/Activity/SettingActivity.java
+++ app/src/main/java/com/example/cctv2/Activity/SettingActivity.java
@@ -2,7 +2,11 @@ package com.example.cctv2.Activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,8 +20,8 @@ import java.util.ArrayList;
 
 public class SettingActivity extends AppCompatActivity {
 
-    private ArrayList<VoiceItem> itemList; // 리스트 데이터 관리
-    private SampleAdapter adapter; // RecyclerView 어댑터
+    private VoiceItem voiceItem;
+    private ImageButton playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,28 +29,27 @@ public class SettingActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_setting);
 
-        // RecyclerView 초기화
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        // ArrayList 및 Adapter 초기화
-        itemList = new ArrayList<>();
-        adapter = new SampleAdapter(itemList);
-        recyclerView.setAdapter(adapter);
+        playButton = findViewById(R.id.btn_play_voice_sample);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //재생 코드
+            }
+        });
 
         // Add 버튼 클릭 이벤트
-        ImageButton btnAddSample = findViewById(R.id.btn_add_sample);
+        ImageButton btnAddSample = findViewById(R.id.refresh_button);
         btnAddSample.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addItemToList();
+                refreshVoice();
             }
         });
     }
 
     // 리스트에 아이템 추가
-    private void addItemToList() {
-        itemList.add(new VoiceItem("Sample"+itemList.size(), null, null, 0)); // 새 아이템 추가
-        adapter.notifyItemInserted(itemList.size() - 1); // RecyclerView 업데이트
+    private void refreshVoice() {
+        Toast t = Toast.makeText(getApplicationContext(), "시연에서는 인터넷 불안정으로 인해\nRefresh가 지원되지 않습니다.",Toast.LENGTH_SHORT);
+        t.show();
     }
 }
