577038a5a3e1f07c99816329ff16aa6ddc897858
Talusor <lth030326@naver.com>
2025-05-17 14:14:07 +0900

    금지 구역 서버 전송 부분 구현

--- app/src/main/java/com/example/cctv2/Activity/SetZoneActivity.java
+++ app/src/main/java/com/example/cctv2/Activity/SetZoneActivity.java
@@ -1,9 +1,11 @@
 package com.example.cctv2.Activity;
 
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.RectF;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -13,10 +15,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.cctv2.R;
 import com.example.cctv2.View.AreaSelectorView;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class SetZoneActivity extends AppCompatActivity {
     ImageView imgView;
     AreaSelectorView selectorView;
     Button saveBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,12 +49,60 @@ public class SetZoneActivity extends AppCompatActivity {
         }
 
         saveBtn.setOnClickListener(v -> {
-            // TODO: Ban Area 구역 저장 구현 할 것
             RectF rect = selectorView.getSelectedRect();
+            RectF normalizedRect = new RectF(
+                    rect.left / imgView.getWidth(),
+                    rect.top / imgView.getHeight(),
+                    rect.right / imgView.getWidth(),
+                    rect.bottom / imgView.getHeight());
+
             Toast.makeText(this,
-                    String.format("(% .2f, % .2f, % .2f, % .2f) Area", rect.left, rect.top, rect.right, rect.bottom),
+                    String.format("(% .2f, % .2f, % .2f, % .2f) Area",
+                            normalizedRect.left,
+                            normalizedRect.top,
+                            normalizedRect.right,
+                            normalizedRect.bottom),
                     Toast.LENGTH_SHORT).show();
 
+            Log.i("SetZoneActivity", "서버로 Area 영역 전송");
+
+            // 저장된 server_url 받아오기
+            SharedPreferences prefs = getSharedPreferences("AppPrefs", MODE_PRIVATE);
+            String url = prefs.getString("server_url", "");
+            if (url.isEmpty()) {
+                Log.e("SetZoneActivity", "Prefs server_url 없음.");
+            } else {
+                // post 형식으로 데이터 서버로 전송
+                OkHttpClient client = new OkHttpClient.Builder().build();
+
+                RequestBody requestBody = new FormBody.Builder()
+                        .add("x1", String.format("%.4f", normalizedRect.left))
+                        .add("y1", String.format("%.4f", normalizedRect.top))
+                        .add("x2", String.format("%.4f", normalizedRect.right))
+                        .add("y2", String.format("%.4f", normalizedRect.bottom))
+                        .build();
+
+                Request request = new Request.Builder()
+                        .url(url + "/video/setting/area")
+                        .post(requestBody) // get , post 등 요청할 HTTP METHOD 작성
+                        .build();
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        Log.e("SetZoneActivity", "요청 실패: " + e.getMessage());
+                    }
+
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) {
+                        if (response.isSuccessful()) {
+                            Log.i("SetZoneActivity", "요청 성공. 응답 코드: " + response.code());
+                        } else {
+                            Log.e("SetZoneActivity", "요청 실패. 응답 코드: " + response.code());
+                        }
+                    }
+                });
+            }
 
             finish();
         });
